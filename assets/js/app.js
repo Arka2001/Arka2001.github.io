ed8fa51508f718db692ca5ab3c533eb69d1bd431
Sayantan <sayantan.dasgupta21@gmail.com>
2021-12-23 23:40:28 +0530

    Updated social media links and added new profile image

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,9 +20,8 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1RKfh6z6rfrks2sCGALI1iwq_YdtOS4v6/view?usp=sharing" target="_blank">Resume</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1oIzLmpPbr-XJfLj7TEBm8ZcxiOSO2XoZ/view?usp=sharing" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
    </li>
